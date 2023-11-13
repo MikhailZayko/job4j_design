306d101817140ff44e06548318284921418dd59f
Admin <mishavolgodonsk@gmail.com>
2023-11-13 04:01:05 +0300

    2. Подключение библиотеки AssertJ [#504881]

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -1,13 +1,16 @@
 package ru.job4j.iterator;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ArrayItTest {
 
     @Test
     void whenMultiCallHasNextThenTrue() {
-        ArrayIt iterator = new ArrayIt(new int[] {1, 2, 3});
+        ArrayIt iterator = new ArrayIt(
+                new int[]{1, 2, 3}
+        );
         boolean result = iterator.hasNext();
         assertThat(result).isTrue();
         assertThat(iterator.hasNext()).isTrue();
@@ -15,7 +18,9 @@ class ArrayItTest {
 
     @Test
     void whenReadSequence() {
-        ArrayIt iterator = new ArrayIt(new int[] {1, 2, 3});
+        ArrayIt iterator = new ArrayIt(
+                new int[]{1, 2, 3}
+        );
         assertThat(iterator.next()).isEqualTo(1);
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.next()).isEqualTo(3);
