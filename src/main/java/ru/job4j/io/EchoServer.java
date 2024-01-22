5d780ab75392bdf4f6cfad42de020e85f793887b
Admin <mishavolgodonsk@gmail.com>
2024-01-22 04:25:04 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,11 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,17 +16,22 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String firstLine = input.readLine();
-                    System.out.println(firstLine);
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        System.out.println(string);
+                    String message = getMessage(input.readLine());
+                    switch (message) {
+                        case "Hello" -> output.write("Hello".getBytes());
+                        case "Exit" -> server.close();
+                        default -> output.write(message.getBytes());
                     }
                     output.flush();
-                    if (firstLine.contains("msg=Bye")) {
-                        server.close();
-                    }
                 }
             }
         }
+    }
+
+    private static String getMessage(String message) {
+        String regex = "msg=(\\S+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(message);
+        return matcher.find() ? matcher.group(1) : "Enter correct data";
     }
 }
